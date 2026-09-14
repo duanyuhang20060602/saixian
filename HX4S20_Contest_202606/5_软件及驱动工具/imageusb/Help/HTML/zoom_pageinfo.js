786aa959fb7626b5a0a133a1f44d1ed621b2e72b
duanyuhang20060602 <duanyuhang20060602@gmail.com>
2026-09-14 23:02:42 +0800

    docs: add original tool readmes and offline help

---
+++ HX4S20_Contest_202606/5_软件及驱动工具/imageusb/Help/HTML/zoom_pageinfo.js
@@ -0,0 +1,10 @@
+pageinfo = [[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null]];
+pagedata = [ ["./contacting_passmark_software.htm","Contacting PassMark&#174; Software","Contacting PassMark&#174; Software Top Previous Australian Head Office: PassMark Software Pty Ltd Level 5, 63 Foveaux St Surry Hills, NSW 2010 Australi...",""],
+["./introduction_and_overview.htm","Introduction and Overview","imageUSB by PassMark&#174; Software - Overview Top Next ImageUSB is a free utility which lets you write an image concurrently to multiple USB Flash Dri...",""],
+["./purchasing_information.htm","Purchasing Information","Purchasing Information Top Previous Next Price imageUSB is free for personal use. What you get when you purchase the software 1.Free Technical sup...",""],
+["./system_requirements.htm","System Requirements","System Requirements Top Previous Next Pentium 4 or newer Windows Vista, Windows 7, 10 and Windows Server 2008 or later 256 Meg RAM. 10MB free hard...",""],
+["./usage.htm","Usage","Using imageUSB Top Previous Next imageUSB&#39;s .bin File Format: imageUSB&#39;s .bin files have a 512-byte header added to the beginning of the raw image...",""]];
